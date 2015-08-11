426180fea368564317fe316b64855a6bc2ed4f07
Logan <student@epicodus.com>
2015-08-10 19:55:08 -0700

    add 2nd test - 3 = 'ping'.

--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -5,7 +5,11 @@
         {
             $number = array();
             for ($index = 1; $index <= $input_number; $index++ ) {
+                if ($index % 3 == 0) {
+                array_push($number, "ping");
+                } else {
                 array_push($number, $index);
+                }
             }
             return $number;
         }
--- tests/PingPongGeneratorTest.php
+++ tests/PingPongGeneratorTest.php
@@ -15,5 +15,18 @@
             //Assert
             $this->assertEquals(array(1, 2), $result);
         }
+
+        function test_makePingPong_ping()
+        {
+            //Arrange
+            $test_PingPongGenerator = new PingPongGenerator;
+            $input = 3;
+
+            //Act
+            $result = $test_PingPongGenerator->generatePingPongArray($input);
+
+            //Assert
+            $this->assertEquals(array(1, 2, "ping"), $result);
+        }
     }
 ?>
